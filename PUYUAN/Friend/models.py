997939f763c8723f440b765fa9ab2875d8a664c3
ArBin <eric91102091@gmail.com>
2023-10-24 20:49:26 +0800

    tmp

--- PUYUAN/Friend/models.py
+++ PUYUAN/Friend/models.py
@@ -1,4 +1,18 @@
 from django.db import models
-
+from User.models import account
+from django.db.models import ForeignKey
 # Create your models here.
-class 
+class Friend(models.Model):
+    user = ForeignKey(account, on_delete=models.CASCADE, related_name='user')
+    friend = models.ForeignKey(account, on_delete=models.CASCADE, related_name='friend', null=True)
+    relation_id = models.IntegerField(default=1)
+    data_type = models.IntegerField(null=True)
+    status = models.IntegerField(default=0)
+    read = models.IntegerField(default=0)
+    code = models.CharField(max_length=100, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
--- PUYUAN/Friend/views.py
+++ PUYUAN/Friend/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+class FriendGetCode(viewsets.ViewSet):
+    def getcode(self, request):
+        pass
+
+
+class FriendGetList(viewsets.ViewSet):
+    def getlist(self, request):
+        pass
+
+class FriendGetRequest(viewsets.ViewSet):
+    def getrequest(self, request):
+        pass
+
+class FriendSend(viewsets.ViewSet):
+    def send(self, request):
+        pass
+
